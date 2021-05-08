9ab666ec3a9775e0271752efce9dd09afc309694
MrVuThanhCong <mr.vtcsharp@gmail.com>
2021-05-08 22:38:21 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', 'top: ' + (e.pageY - 10)
+    + 'px; left: ' + (e.pageX + 20) + 'px;');
+})
+
+document.addEventListener('click', e => {
+    cursor.classList.add("expand"); 
+
+    setTimeout(() => {
+        cursor.classList.remove("expand")
+    }, 500)
+})
